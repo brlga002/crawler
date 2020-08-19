c2870244fa0b55ebbf70793f7f21515e79933701
brlga002 <gabriel.lima1000@gmail.com>
2020-08-19 16:58:23 -0400

    script

---
+++ clonaTudo.sh
@@ -0,0 +1,16 @@
+//https://api.github.com/users/brlga002/repos
+git clone git@github.com:brlga002/aulaLaravel.git
+git clone git@github.com:brlga002/api-moongose.git
+git clone git@github.com:brlga002/nutrisucos.git
+git clone git@github.com:brlga002/beTheHero.git
+git clone git@github.com:brlga002/brlga002.git
+git clone git@github.com:brlga002/chat-radiologia.git
+git clone git@github.com:brlga002/crawler.git
+git clone git@github.com:brlga002/devmanaus.git
+git clone git@github.com:brlga002/digistarts.git
+git clone git@github.com:brlga002/fiscal-offline.git
+git clone git@github.com:brlga002/instagram-clone.git
+git clone git@github.com:brlga002/listagemProjetos.git
+git clone git@github.com:brlga002/memorando-economico.git
+git clone git@github.com:brlga002/nubank-clone.git
+git clone git@github.com:brlga002/oficio-memorando.git
